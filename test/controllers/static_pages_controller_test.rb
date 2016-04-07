1f02cb1d48373f43226e945a47704ae8dddfdf71
Bryan Garza <bgarza1016@gmail.com>
2016-04-07 00:09:19 -0700

    Added contact page with tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -19,4 +19,10 @@ class StaticPagesControllerTest < ActionController::TestCase
   	assert_select "title", "About | Joborg"
   end
 
+  test "should get contact" do
+  	get :contact
+  	assert_response :success
+  	assert_select "title", "Contact | Joborg"
+  end
+
 end
