c0c41ff1226fe40c504eb89062c59b64096e7dde
Bryan Garza <bgarza1016@gmail.com>
2016-04-11 14:51:13 -0700

    Middle - basic user page and an all users page done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
+  def index
+    @users = User.all
+  end
 
 	def show
 		@user = User.find(params[:id])
@@ -13,10 +19,24 @@ class UsersController < ApplicationController
   	if @user.save
   		log_in @user
   		flash[:success] = "Welcome to Joborg!"
-  		redirect_to @user
+  		redirect_back_or @user
   	else
   		render 'new'
   	end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   private
@@ -24,4 +44,21 @@ class UsersController < ApplicationController
   def user_params
   	params.require(:user).permit(:name, :email, :password, :password_confirmation)
 	end
+
+  # Before filters.
+
+  # Confirms a logged_in user.
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
+  # Confirms the correct user.
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless @user == current_user
+  end
 end
